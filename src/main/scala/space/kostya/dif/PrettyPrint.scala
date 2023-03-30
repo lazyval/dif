a58cad189f8db0c38dbec3294984f3a101862d80
Kostya Golikov <lazyval@users.noreply.github.com>
2023-03-31 01:33:32 +0200

    Add impl + test for plaintext format

--- src/main/scala/space/kostya/dif/PrettyPrint.scala
+++ src/main/scala/space/kostya/dif/PrettyPrint.scala
@@ -8,9 +8,17 @@ trait PrettyPrint {
 
 object PlainText extends PrettyPrint {
   override def format(firstId: String, secondId: String, diffs: List[Op]): List[String] = {
-    val header = List(s"--- $firstId", s"+++ $secondId")
-    val lines  = diffs.map(_.toString)
+    val header = List(s"$firstId|$secondId|")
+    val lines  = diffs.map(strOp)
     header ++ lines
+  }
+  def strOp(op: Op): String = op match {
+    case Add(path, value)  => s"$path -> <none>|+$value|"
+    case Remove(path, old) => s"$path -> -$old|<none>|"
+    case Replace(path, value, old) =>
+      s"$path -> -$old|+$value|"
+    case Move(from, to) => s"$from => $to|"
+    case Copy(from, to) => s"$from => $to|"
   }
 }
 
--- src/test/scala/space/kostya/dif/PrettyPrintSpec.scala
+++ src/test/scala/space/kostya/dif/PrettyPrintSpec.scala
@@ -20,4 +20,18 @@ class PrettyPrintSpec extends AnyFlatSpec with Matchers {
          |    └── /baz
          |""".stripMargin
   }
+
+  it should "print out all the values as is in simplified format" in {
+    val xs = List(
+      Replace("/foo", "yyy", "xxx"),
+      Replace("/foo/bar", "yyy", "xxx"),
+      Replace("/foo/bar/baz", "yyy", "xxx")
+    )
+    val lines: Seq[String] = PlainText.format("xxx", "yyy", xs)
+    lines.mkString("\n") shouldEqual
+      """|xxx|yyy|
+         |/foo -> -xxx|+yyy|
+         |/foo/bar -> -xxx|+yyy|
+         |/foo/bar/baz -> -xxx|+yyy|""".stripMargin
+  }
 }
