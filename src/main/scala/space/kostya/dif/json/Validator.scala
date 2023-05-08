a4d7639d0ad5fe41fea958b4cb6fadb5d54fa38b
Kostya Golikov <lazyval@users.noreply.github.com>
2023-05-08 23:52:28 +0200

    Rename + reuse resource ops

--- src/main/scala/space/kostya/dif/json/Validator.scala
+++ src/main/scala/space/kostya/dif/json/Validator.scala
@@ -4,32 +4,30 @@ import cats.data.Validated
 import cats.data.Validated.{Invalid, Valid}
 import com.github.erosb.jsonsKema.*
 import io.circe.Json
-import space.kostya.dif.ValidatingParser
+import space.kostya.dif.ResourceOps
 
-trait ValidatingParser {
+trait Validator {
   def parse(rawString: String): Validated[Exception, Json]
 }
 
-object SkemaValidation extends ValidatingParser {
+object SkemaValidator extends Validator {
 
   val rawSchema = {
     import scala.io.Source
-    val path = "dataflow_job.schema"
+    val path         = "dataflow_job.schema"
     val schemaStream = getClass.getClassLoader.getResourceAsStream(path)
-    val schemaSource = Source.fromInputStream(schemaStream)
-    val schemaString = schemaSource.mkString
-    schemaSource.close()
-    schemaString
+    ResourceOps.tryWithResource(Source.fromInputStream(schemaStream)) { schemaSource =>
+      schemaSource.mkString
+    }
   }
 
-
   val schemaJson: JsonValue = new JsonParser(rawSchema).parse()
-  val schema = new SchemaLoader(schemaJson).load()
-  val validator = Validator.forSchema(schema)
+  val schema                = new SchemaLoader(schemaJson).load()
+  val validator             = Validator.forSchema(schema)
 
   override def parse(rawJson: String): Validated[Exception, Json] = {
     val skemaJson: JsonValue = new JsonParser(rawJson).parse()
-    val circeJson = io.circe.parser.parse(rawJson)
+    val circeJson            = io.circe.parser.parse(rawJson)
 
     val failure: ValidationFailure = validator.validate(skemaJson)
     if (failure == null) Valid(circeJson.right.get)
--- src/test/scala/space/kostya/dif/json/SkemaValidatorSpec.scala
+++ src/test/scala/space/kostya/dif/json/SkemaValidatorSpec.scala
@@ -3,12 +3,12 @@ package space.kostya.dif.json
 import org.scalatest.TryValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import space.kostya.dif.json.SkemaValidation
+import space.kostya.dif.json.SkemaValidator
 
-class SkemaValidationSpec extends AnyFlatSpec with Matchers {
+class SkemaValidatorSpec extends AnyFlatSpec with Matchers {
   "validator based on skema lib" should "return exception for empty json" in {
-    val json = "{}"
-    val result = SkemaValidation.parse(json)
+    val json   = "{}"
+    val result = SkemaValidator.parse(json)
     result.isInvalid should equal(true)
   }
 }
