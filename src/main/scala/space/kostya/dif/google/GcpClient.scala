f55c11601c53cee1c2104f5173fedf61a823e587
Kostya Golikov <lazyval@users.noreply.github.com>
2023-05-16 18:54:28 +0200

    Sketch shell implementation for gcp calls

--- src/main/scala/space/kostya/dif/google/GcpClient.scala
+++ src/main/scala/space/kostya/dif/google/GcpClient.scala
@@ -1,9 +1,14 @@
 package space.kostya.dif.google
 
-import com.google.dataflow.v1beta3.{Environment, Job}
-import org.threeten.bp.ZoneId
+import com.typesafe.scalalogging.LazyLogging
 import space.kostya.dif.Api
 import space.kostya.dif.model.{JobDescription, JobEnvironment, JobSummary}
+import io.circe.Decoder.Result
+import io.circe.DecodingFailure.Reason.CustomReason
+import io.circe.{Decoder, DecodingFailure}
+import io.circe.generic.auto.*
+import io.circe.parser.*
+import space.kostya.dif.json.Parser
 
 import java.time
 import java.time.LocalDateTime
@@ -12,25 +17,25 @@ import scala.util.Try
 // stab for client based on google java apis use
 // spent too much time on this, trying to fix propagation of credentials
 // will switch over to process based wrapper over gcloud cli for now
-class GcpClient(projectId: String) extends Api {
-  override def listJobs(): Try[List[JobSummary]]               = ???
-  override def describeJob(jobId: String): Try[JobDescription] = ???
-}
+class GcpClient(projectId: String, region: String) extends Api with LazyLogging {
 
-given Conversion[Job, JobSummary] with {
-  def apply(job: Job): JobSummary = {
-    val createTime: LocalDateTime = {
-      val secs = job.getCreateTime.getSeconds
-      LocalDateTime.ofEpochSecond(secs, job.getCreateTime.getNanos, time.ZoneOffset.UTC)
-    }
+  import scala.sys.process._
 
-    JobSummary(
-      id = job.getId,
-      name = job.getName,
-      projectId = job.getProjectId,
-      `type` = job.getType.toString,
-      currentState = job.getCurrentState.toString,
-      createTime = createTime
-    )
+  private def describeCmd(jobId: String) = {
+    // <!> taking the raw input is unsafe due to potential injection, but
+    // assuming we're running in trusted environment
+    Seq(
+      "gcloud dataflow jobs describe",
+      s"$jobId --project=$projectId",
+      "--format=json",
+      s"--region $region --full"
+    ).mkString(" ")
   }
+
+  override def listJobs(): Try[List[JobSummary]] = ???
+
+  override def describeJob(jobId: String): Try[JobDescription] = Try {
+    val cmd = describeCmd(jobId)
+    cmd.!!
+  }.flatMap(output => Parser.readDescription(output))
 }
